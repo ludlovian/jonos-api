67b978d2c8be58eb4590f13318e9f0bf49905d09
Alan Layng <alan.layng@gmail.com>
2024-06-07 10:47:58 +0100

    Now ready to test

--- src/config.mjs
+++ src/config.mjs
@@ -1,23 +1,8 @@
 import process from 'node:process'
 
 import { parse as parseMs } from '@lukeed/ms'
-import camelCase from 'pixutil/camelCase'
-
-import convert from './convert.mjs'
-
-function envVars (prefix) {
-  return Object.fromEntries(
-    Object.entries(process.env)
-      .filter(([key]) => key.startsWith(prefix))
-      .map(([key, value]) => [camelCase(key.slice(prefix.length)), value])
-  )
-}
-
-function convertObject (o) {
-  return Object.fromEntries(
-    Object.entries(o).map(([key, value]) => [key, convert(value)])
-  )
-}
+import camelCase from 'pixutil/camel-case'
+import guess from 'pixutil/guess'
 
 const defaults = {
   apiCallRetryCount: 1,
@@ -29,6 +14,20 @@ const defaults = {
   apiSubscriptionRenew: parseMs('25m'),
 
   apiDiscoveryTimeout: parseMs('3s')
+}
+
+function envVars (prefix) {
+  return Object.fromEntries(
+    Object.entries(process.env)
+      .filter(([key]) => key.startsWith(prefix))
+      .map(([key, value]) => [camelCase(key.slice(prefix.length)), value])
+  )
+}
+
+function convertObject (o) {
+  return Object.fromEntries(
+    Object.entries(o).map(([key, value]) => [key, guess(value)])
+  )
 }
 
 export default {
--- src/parseElement.mjs
+++ src/parseElement.mjs
@@ -1,5 +1,5 @@
-import camelCase from 'pixutil/camelCase'
-import convert from './convert.mjs'
+import camelCase from 'pixutil/camel-case'
+import guess from 'pixutil/guess'
 
 export default function parseElement (elem) {
   return Object.fromEntries(
@@ -8,7 +8,7 @@ export default function parseElement (elem) {
         if (typeof child === 'string') return undefined
         const key = camelCase(child.type)
         const value =
-          typeof child.children[0] === 'string' ? convert(child.text) : child
+          typeof child.children[0] === 'string' ? guess(child.text) : child
         return [key, value]
       })
       .filter(Boolean)
