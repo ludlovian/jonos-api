fbc820aa6d88874a9d0e1b40054c71d0271feb21
Alan Layng <alan.layng@gmail.com>
2024-06-21 11:50:36 +0100

    add WEB constant

--- src/constants.mjs
+++ src/constants.mjs
@@ -3,6 +3,7 @@ const QUEUE = 'x-rincon-queue:'
 const RADIO = 'x-rincon-mp3radio:'
 const TV = 'x-sonos-htastream:'
 const CIFS = 'x-file-cifs:'
+const WEB = 'https:'
 
-export default { FOLLOW, QUEUE, RADIO, TV, CIFS }
-export { FOLLOW, QUEUE, RADIO, TV, CIFS }
+export default { FOLLOW, QUEUE, RADIO, TV, CIFS, WEB }
+export { FOLLOW, QUEUE, RADIO, TV, CIFS, WEB }
